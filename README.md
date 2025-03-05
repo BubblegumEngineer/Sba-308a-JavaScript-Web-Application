2021831dcab2ebd41181dc53f73b365f5bfdf5d7
BubblegumEngineer <temmanuel721@gmail.com>
2025-03-05 01:54:35 -0500

    second commit - added CSS, buttons, search bar

---
+++ README.md
@@ -0,0 +1,4 @@
+# JavaScript Web Application Weather App Project
+
+## Description
+This is a simple JavaScript project that demonstrates how to create a weather app using a weather app API.
--- index.js
+++ index.js
@@ -1,36 +1,53 @@
-// Current Weather API Endpoint
+function getWeather() {
+
+    const apiKey = 'eb17e33984fe1a26da9b07f48293aed6';
+    const city = document.getElementById('city').value;
+    
+    if (!city) {
+        alert('Please enter a city');
+        return;
+    }
+
+    const currentWeatherUrl = `https://api.weatherstack.com/current?access_key=eb17e33984fe1a26da9b07f48293aed6&query=${city}`; // see 13:04 min if you wanna change Api
+
+}
+
+
+
+
+//=============================================
 
 // "http://api.weatherstack.com/current"
     // ? access_key = eb17e33984fe1a26da9b07f48293aed6
 //     & query = New York
 
-    const baseUrl= "http://api.weatherstack.com/"
+    // const baseUrl= "http://api.weatherstack.com/"
     
 // optional parameters: 
 
     // & units = m
     // & language = en
     // & callback = MY_CALLBACK
-
-const url = "https://api.weatherstack.com/current?access_key=eb17e33984fe1a26da9b07f48293aed6&query=Brooklyn";
+// let city = "Brooklyn";
+// const url = `https://api.weatherstack.com/current?access_key=eb17e33984fe1a26da9b07f48293aed6&query=${city}`;
 // const location = "New York"
 
-const options = {
-	method: 'GET'
+// const options = {
+// 	method: 'GET'
 
-};
+// };
 
 
-async function fetchWeather() {
-    try {
-        const response = await fetch(url, options);
+// async function fetchWeather() {
+//     try {
+//         const response = await fetch(url, options);
        
-        const data = await response.json() // added
-console.log(data) // added
+//         const data = await response.json() // added
+// console.log(data) // added
 
-    } catch (error) {
-        console.error(error);
-    }
-}
+//     } catch (error) {
+//         console.error(error);
+//     }
+// }
 
-fetchWeather();
+// fetchWeather();
